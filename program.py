f4326c88a1bce6eed9ac84bf3b5d6a1ddba216cb
Dulhan <dhjayalath@gmail.com>
2019-08-07 18:50:51 +0100

    5 predictions made after training for demo

--- program.py
+++ program.py
@@ -11,13 +11,6 @@ with np.load('mnist.npz') as data:
     validation_images = data['validation_images']
     validation_labels = data['validation_labels']
 
-# # Format each data set as tuples (x, y) where x is an array containing
-# # the brightness values of the image and y is an array of length 10 
-# # where 1.0 represents the index of the number the image represents
-# training_data = [(x, y) for x, y in zip(training_images, training_labels)]
-# test_data = [(x, y) for x, y in zip(test_images, test_labels)]
-# validation_data = [(x, y) for x, y in zip(validation_images, validation_labels)]
-
 # Reformat arrays to work with matrix system in network
 training_images = np.array(np.reshape(training_images, (len(training_images), 784)))
 training_labels = np.array(np.reshape(training_labels, (len(training_labels), 10)))
@@ -28,12 +21,28 @@ validation_images = np.array(np.reshape(validation_images, (len(validation_image
 
 # Train network
 net = Network([784, 30, 10], training_images, training_labels)
-net.SGD(30, 1.0, 10, test_images=test_images, test_labels=test_labels)
+net.SGD(5, 1.0, 10, test_images=test_images, test_labels=test_labels)
 
-# Make random guesses to demonstrate accuracy until cancelled
-while True:
+
+# Make 5 predictions using trained network for demonstration
+images = np.zeros((5, 784))
+guesses = []
+confidences = []
+for _ in range(5):
     i = random.randint(0, len(validation_images) - 1)
-    plt.imshow(validation_images[i].reshape(28, 28), cmap='gray')
-    plt.show()
-    print("Actual: " + str(np.argmax(validation_labels[i])))
-    print("Guess: " + str(np.argmax(net.feedforward(validation_images[i]))))
+    images[_] = validation_images[i]
+    output = net.feedforward(validation_images[i])
+    guess = np.argmax(output)
+    confidence = round((output[0][guess] / np.sum(output)) * 100.0, 2)
+    guesses.append(guess)
+    confidences.append(confidence)
+
+plt.imshow(images.reshape(140, 28), cmap='gray')
+
+for i in range(5):
+    plt.annotate(str(guesses[i]), xy=(0, 0), xytext=(35, 17 + 28 * i), fontsize=20, color='k')
+    plt.annotate(str(confidences[i]) + "%", xy=(0, 0), xytext=(50, 17 + 28 * i), fontsize=20)
+
+plt.xticks([])
+plt.yticks([])
+plt.show()
