88c853ecacb2bd7688eb64e7dce49b727a9e36f2
Dulhan <dhjayalath@gmail.com>
2019-08-10 13:35:06 +0100

    Early stopping and optimized learning rate

--- program.py
+++ program.py
@@ -21,7 +21,7 @@ validation_images = np.array(np.reshape(validation_images, (len(validation_image
 
 # Train network
 net = Network([784, 30, 10], training_images, training_labels, test_images, test_labels, validation_images, validation_labels)
-net.SGD(30, 0.5, 20, monitor_text=True)
+net.SGD(15, 0.9329610181548651, 20, monitor_text=True, optimizing=False)
 
 # Make 5 predictions using trained network for demonstration
 images = np.zeros((5, 784))
